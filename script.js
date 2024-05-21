a51e78364b07b57c80fc69802f80a3c89406b2a6
sethdimmer234 <sethdimmer1@gmail.com>
2024-05-21 11:39:20 -0400

    initial commit

--- script.js
+++ script.js
@@ -5,5 +5,11 @@ window.addEventListener('scroll', fixNav)
 function fixNav() {
     console.log(window.scrollY, nav.offsetHeight)
     // need to figure out when to turn the active class on when i scroll
-
+    if(window.scrollY > nav.offsetHeight + 150){
+        // if window scrollY position is greater than the nav offsetHeight i will add 150
+        nav.classList.add('active')
+    }
+    else{
+        nav.classList.remove('active')
+    }
 }
